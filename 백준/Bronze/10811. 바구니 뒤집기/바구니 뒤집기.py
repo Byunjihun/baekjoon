ae4947153de61adffd5c94e5dbade88711b04ffd
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-04-30 20:58:21 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,16 +1,15 @@
-N,M = map(int, input().split())
+N,M = map(int,input().split())
 
-baskets = list(range(1,N+1))
+numbers = list(range(1,N+1))
 
-def revers_baskets(start,end):
-    while(start < end):
-        baskets[start-1],baskets[end-1] = baskets[end-1], baskets[start-1]
-        start +=1
-        end -= 1
-    
+def reversed(num1, num2):
+    while num1 < num2:
+        numbers[num1 - 1] ,numbers[num2 - 1] = numbers[num2 - 1] , numbers[num1 - 1]
+        num1 += 1
+        num2 -= 1
 
 for _ in range(M):
-    i,j = map(int, input().split())
-    revers_baskets(i,j)
-    
-print(*baskets)
+    i,j = map(int,input().split())
+    reversed(i,j)
+
+print(*numbers)
