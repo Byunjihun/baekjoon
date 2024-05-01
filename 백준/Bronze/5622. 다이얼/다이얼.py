3d47fa4b7a3524240989c89631cbfc66f5e7f353
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-05-01 15:24:15 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,12 +1,9 @@
-word = input()
+dial = {'A':2, 'B':2, 'C':2, 'D':3, 'E':3,'F':3,'G':4,'H':4,'I':4,'J':5,'K':5,'L':5,'M':6,'N':6,'O':6,'P':7,'Q':7,'R':7,'S':7,'T':8,'U':8,'V':8,'W':9,'X':9,'Y':9,'Z':9}
 
-dial = {'A':2, 'B':2, 'C':2, 'D':3, 'E':3, 'F':3, 'G':4, 'H':4, 'I':4, 'J':5, 'K':5, 'L':5,
-        'M':6, 'N':6, 'O':6, 'P':7, 'Q':7, 'R':7, 'S':7, 'T':8, 'U':8, 'V':8, 'W':9, 'X':9,
-        'Y':9, 'Z':9}
+numbers = list(input())
 
-time = 0
-
-for char in word:
-    time += dial[char] + 1 
-
-print(time)
+sum_dial = 0
+for i in numbers:
+    if i in dial:
+        sum_dial += dial[i] + 1
+print(sum_dial)
