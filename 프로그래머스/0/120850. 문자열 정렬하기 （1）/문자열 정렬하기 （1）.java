9e0ec5ba0b19cd6b2b55638ec6dc95f3c9d4c518
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-07-18 13:24:26 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,14 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String my_string) {
-       my_string = my_string.replaceAll("[a-z]", "");
+        my_string = my_string.replaceAll("[^0-9]","");
+        System.out.println(my_string);
 
         String[] arr = my_string.split("");
+
         int[] answer = new int[arr.length];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i=0; i<arr.length; i++){
             answer[i] = Integer.parseInt(arr[i]);
         }
+
         Arrays.sort(answer);
 
         return answer;
