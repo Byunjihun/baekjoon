32a465400bf894cd922ba11c76a3f99c31204b16
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-08-02 14:52:41 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.58 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,9 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] numbers) {
-         Arrays.sort(numbers);
-        int num1 = numbers[0] * numbers[1];
-        int num2 = numbers[numbers.length -1] * numbers[numbers.length -2];
-        return num1 > num2 ? num1 : num2;
+        int answer = 0;
+        Arrays.sort(numbers);
+        return numbers[0] * numbers[1] > numbers[numbers.length -1] * numbers[numbers.length -2] ? numbers[0] * numbers[1] : numbers[numbers.length -1] * numbers[numbers.length -2];
     }
 }
