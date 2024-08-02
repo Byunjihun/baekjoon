74ddd4787f5e71c4a32ff22b689969a74dd5a7fa
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-08-02 14:49:39 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,32 +1,35 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-       int[] answer = {0,0};
-        int a = board[0] / 2;
-        int b = board[1] / 2;
+         int[] answer = {0,0};
+        int lenth1 = board[0] / 2;
+        int lenth2 = board[1] / 2;
 
-        for(int i = 0; i < keyinput.length; i++) {
-            if(keyinput[i].equals("left")) {
+        for (int i=0; i< keyinput.length; i++){
+            if (keyinput[i].equals("left")){
                 answer[0]--;
-                if(answer[0] < -a)
-                    answer[0] = -a;
-
-            } else if (keyinput[i].equals("right")) {
+                if (answer[0] <= -lenth1){
+                    answer[0] = -lenth1;
+                }
+            }
+            if (keyinput[i].equals("right")){
                 answer[0]++;
-                if(answer[0] > a)
-                    answer[0] = a;
-
-            } else if (keyinput[i].equals("up")) {
+                if (answer[0] >= lenth1){
+                    answer[0] = lenth1;
+                }
+            }
+            if (keyinput[i].equals("up")){
                 answer[1]++;
-                if(answer[1] > b)
-                    answer[1] = b;
-
-            } else {
+                if (answer[1] >= lenth2){
+                    answer[1] = lenth2;
+                }
+            }
+            if (keyinput[i].equals("down")){
                 answer[1]--;
-                if(answer[1] < -b)
-                    answer[1] = -b;
+                if (answer[1] <= -lenth2){
+                    answer[1] = -lenth2;
+                }
             }
         }
-
         return answer;
     }
 }
