6c018ff1a5dc1865b0c98ed58a3471d08c4e54ee
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-08-20 16:38:15 +0900

    [level 0] Title: 특이한 정렬, Time: 0.88 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,17 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numlist, int n) {
-            for (int i = 0; i < numlist.length - 1; i++) {
-            for (int j = 0; j < numlist.length - 1 - i; j++) {
-                int distance1 = Math.abs(n - numlist[j]);
-                int distance2 = Math.abs(n - numlist[j + 1]);
-                
-                if (distance1 > distance2 || (distance1 == distance2 && numlist[j] < numlist[j + 1])) {
-                    int temp = numlist[j];
-                    numlist[j] = numlist[j + 1];
-                    numlist[j + 1] = temp;
+        Arrays.sort(numlist);
+        
+        for (int i = 0; i < numlist.length; i++) {
+            for (int j = 0; j < numlist.length; j++) {
+                if (Math.abs(numlist[i] - n) <= Math.abs(numlist[j] - n)) {
+                    int temp = numlist[i];
+                    numlist[i] = numlist[j];
+                    numlist[j] = temp;
                 }
             }
         }
+        
         return numlist;
     }
 }
