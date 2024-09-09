6bbf14b01dc01cdfa4170514246198797680a100
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-09-09 18:08:58 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
@@ -15,22 +14,22 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] arr1 = new int[N];
+        Integer[] arr1 = new Integer[N];
         Integer[] arr2 = new Integer[N];
 
-        for (int i=0; i<arr1.length; i++){
+        for (int i=0; i<N; i++){
             arr1[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<arr2.length; i++){
+        for (int i=0; i<N; i++){
             arr2[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2,Comparator.reverseOrder());
+        Arrays.sort(arr1, Comparator.naturalOrder());
+        Arrays.sort(arr2, Comparator.reverseOrder());
 
-        int sum = 0;
+        int sum=0;
         for (int i=0; i<N; i++){
             sum += arr1[i] * arr2[i];
         }
