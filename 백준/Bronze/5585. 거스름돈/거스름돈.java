4f24bb1d38487b602789b777c0a9e41528e2bbca
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-09-10 13:25:09 +0900

    [Bronze II] Title: 거스름돈, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] coins = {500,100,50,10,5,1};
-        int money = Integer.parseInt(br.readLine());
-        int change = 1000-money;
-        int count = 0;
+        int Money = Integer.parseInt(br.readLine());
+        int Change = 1000 - Money;
+        int[] change_money = new int[]{500,100,50,10,5,1};
 
-        for (int i=0; i<coins.length; i++){
-            if (change / coins[i] > 0){
-                count += change / coins[i];
-                change = change % coins[i];
+        int cnt = 0;
+        for (int i=0; i<change_money.length; i++){
+            if (Change / change_money[i] > 0){
+                cnt += Change / change_money[i];
+                Change = Change % change_money[i];
             }
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
