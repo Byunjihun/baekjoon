1c962e357aaffc6950cf89fa3951c95210118385
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-09-16 14:03:24 +0900

    [Bronze I] Title: 캠핑, Time: 156 ms, Memory: 16388 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.java
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -4,11 +4,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int i = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int cal = 0;
         while (true){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int L = Integer.parseInt(st.nextToken());
@@ -19,9 +19,11 @@ public class Main {
                 break;
             }
 
+            int sum = 0;
             i++;
-            cal = ((V / P) * L) + Math.min(V % P, L);
-            System.out.println("Case " + i + ": " + cal);
+            sum += ((V / P) * L) + Math.min((V % P),L);
+
+            System.out.println("Case " + i + ": " + sum);
         }
     }
 }
