7b4c2ba928d14fb273717bf8d5a811a934e9b83e
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-09-30 22:28:53 +0900

    [D1] Title: 연월일 달력, Time: 128 ms, Memory: 20,392 KB -BaekjoonHub

---
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String str = sc.next();
+            String year = str.substring(0, 4);
+            String month = str.substring(4, 6);
+            String day = str.substring(6, 8);
+
+            if (Integer.parseInt(month) > 0 && Integer.parseInt(month) <= 12 && Integer.parseInt(day) >= 1 && Integer.parseInt(day) <= days[Integer.parseInt(month) - 1]) {
+                System.out.println("#" + test_case + " " + year + "/" + month + "/" + day);
+            } else {
+                System.out.println("#" + test_case + " -1");
+            }
+        }
+    }
+}
