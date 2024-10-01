6fbb5246f045b247a749f4993d01a732a131f665
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-10-01 21:47:31 +0900

    [D1] Title: 신문 헤드라인, Time: 130 ms, Memory: 20,240 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String upperCase = str.toUpperCase();
+        System.out.println(upperCase);
+    }
+}
