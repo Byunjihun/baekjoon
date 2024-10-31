043fbc1b59aec625ead799d94be9bad95d0e5a9d
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-10-31 15:19:08 +0900

    [D2] Title: 숫자를 정렬하자, Time: 204 ms, Memory: 20,832 KB -BaekjoonHub

---
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            int[] list = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                list[i] = sc.nextInt();
+            }
+
+            Arrays.sort(list);
+
+            System.out.print("#" + test_case + " ");
+            for (int i = 0; i < list.length; i++) {
+                System.out.print(list[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
