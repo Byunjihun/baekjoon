e7775868e161de25bf08cc1123cb48db849f53fc
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-11-16 15:24:23 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 2,126 ms, Memory: 133,076 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -9,24 +9,22 @@ public class Solution {
 
         for (int test_case = 1; test_case <= T; test_case++) {
             int N = sc.nextInt();
-            int[] days = new int[N];
+            int[] list = new int[N];
 
-            for (int i = 0; i < N; i++) {
-                days[i] = sc.nextInt();
+            for (int i = 0; i < list.length; i++) {
+                list[i] = sc.nextInt();
             }
 
+            int max = 0;
             long sum = 0;
-            int max = Integer.MIN_VALUE;
-
             for (int i = N - 1; i >= 0; i--) {
-                if (max < days[i]) {
-                    max = days[i];
-                } else {
-                    sum += (max - days[i]);
+                if (max < list[i]) {
+                    max = list[i];
                 }
+
+                sum += max - list[i];
             }
             System.out.println("#" + test_case + " " + sum);
         }
-        sc.close();
     }
 }
