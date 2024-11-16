534dc7f00699bee46fc5afa4a78d62c692855e00
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-11-16 18:39:45 +0900

    [D2] Title: 스도쿠 검증, Time: 156 ms, Memory: 23,096 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -5,7 +6,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-
         for (int test_case = 1; test_case <= T; test_case++) {
             int[][] sudoku = new int[9][9];
 
@@ -15,57 +15,51 @@ public class Solution {
                 }
             }
 
-            boolean isValid = true;
+            boolean Value = true;
 
             for (int i = 0; i < 9; i++) {
-                if (!checkNum(sudoku[i])) {
-                    isValid = false;
-                    break;
+                if (!Check_num(sudoku[i])) {
+                    Value = false;
                 }
             }
 
-            if (isValid) {
-                for (int i = 0; i < 9; i++) {
-                    int[] col = new int[9];
-                    for (int j = 0; j < 9; j++) {
-                        col[j] = sudoku[j][i];
-                    }
-                    if (!checkNum(col)) {
-                        isValid = false;
-                        break;
-                    }
+            int[][] col_sudoku = new int[9][9];
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    col_sudoku[i][j] = sudoku[j][i];
+                }
+                if (!Check_num(col_sudoku[i])) {
+                    Value = false;
                 }
             }
+            int[] block_sudoku = new int[9];
+            for (int i = 0; i < 9; i += 3) {
+                for (int j = 0; j < 9; j += 3) {
+                    int c = 0;
 
-            if (isValid) {
-                for (int i = 0; i < 9; i += 3) {
-                    for (int j = 0; j < 9; j += 3) {
-                        int[] block = new int[9];
-                        int idx = 0;
-                        for (int x = i; x < i + 3; x++) {
-                            for (int y = j; y < j + 3; y++) {
-                                block[idx++] = sudoku[x][y];
-                            }
-                        }
-                        if (!checkNum(block)) {
-                            isValid = false;
-                            break;
+                    for (int x = 0; x < 3; x++) {
+                        for (int y = 0; y < 3; y++) {
+                            block_sudoku[c] = sudoku[i + x][j + y];
+                            c++;
                         }
                     }
+                    if (!Check_num(block_sudoku)) {
+                        Value = false;
+                    }
                 }
             }
-
-            System.out.printf("#%d %d\n", test_case, isValid ? 1 : 0);
+            System.out.println("#" + test_case + " " + (Value ? 1 : 0));
         }
     }
 
-    private static boolean checkNum(int[] nums) {
-        boolean[] seen = new boolean[10];
-        for (int num : nums) {
-            if (seen[num]) {
+    private static boolean Check_num(int[] numbers) {
+        boolean[] list = new boolean[10];
+        for (int i = 1; i < numbers.length; i++) {
+            if (!list[numbers[i]]) {
+                list[numbers[i]] = true;
+            } else {
                 return false;
             }
-            seen[num] = true;
         }
         return true;
     }
