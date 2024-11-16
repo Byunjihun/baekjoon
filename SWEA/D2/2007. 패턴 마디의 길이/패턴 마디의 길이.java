9ab1874feb01b11dda05170c74f527774eb71b10
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-11-16 18:59:48 +0900

    [D2] Title: 패턴 마디의 길이, Time: 186 ms, Memory: 20,432 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -5,23 +5,22 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        sc.nextLine();
 
-        for (int t = 1; t <= T; t++) {
-            String str1 = sc.nextLine();
-            int length = str1.length();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String str1 = sc.next(); //KOREAKOREAKOREAKOREAKOREAKOREA
+            int length = str1.length(); // 30
             int patternLength = 0;
 
             for (int i = 1; i <= Math.min(length / 2, 10); i++) {
                 String str2 = str1.substring(0, i);
                 String str3 = str1.substring(i, i + i);
-                if (str1.length() >= (i + i) && str2.equals(str3)) {
+                if ( str2.equals(str3)) {
                     patternLength = str2.length();
                     break;
                 }
             }
 
-            System.out.println("#" + t + " " + patternLength);
+            System.out.println("#" + test_case + " " + patternLength);
         }
     }
 }
