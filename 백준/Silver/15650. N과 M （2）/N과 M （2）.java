90aa9885d1652e1b212c13269fb1673a27021c77
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2024-12-03 14:26:32 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 16236 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sequence = new int[M];
+
+        backtrak(0, 1);
+    }
+
+    private static void backtrak(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(sequence[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            sequence[depth] = i;
+            backtrak(depth + 1, i + 1);
+        }
+    }
+}
