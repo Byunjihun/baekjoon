fc10b9ccf5cbfdb5cd5257c09a16cefbf25cbdfc
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2025-01-12 21:04:39 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,31 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] str = br.readLine().split(" ");
 
-        int count = 0;
+        int[] numbers = new int[N];
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            boolean check = check(num);
+            numbers[i] = Integer.parseInt(str[i]);
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            boolean check = check(numbers[i]);
             if (check) count++;
         }
+
         System.out.println(count);
     }
 
     private static boolean check(int a) {
-        if (a < 2) return false;
+        if (a < 2) {
+            return false;
+        }
         for (int i = 2; i <= Math.sqrt(a); i++) {
-            if (a % i == 0){
+            if (a % i == 0) {
                 return false;
             }
         }
