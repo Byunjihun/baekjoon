f84790a16d511e47d629f8afd228ae42fdc2d7d1
Byunjihun <91594844+Byunjihun@users.noreply.github.com>
2025-01-12 18:32:44 +0900

    [Gold IV] Title: 약수의 합, Time: 400 ms, Memory: 44984 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.java
+++ 백준/Gold/17425. 약수의 합/약수의 합.java
@@ -3,31 +3,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int MAX = 1000000;
+        int max = 1000000;
 
-        long[] f = new long[MAX + 1];
-        long[] g = new long[MAX + 1];
+        long[] f = new long[max + 1];
+        long[] g = new long[max + 1];
 
-        for (int i = 1; i <= MAX; i++) {
-            for (int j = i; j <= MAX; j += i) {
+        for (int i = 1; i <= max; i++) {
+            for (int j = i; j <= max; j += i) {
                 f[j] += i;
             }
         }
 
-        for (int i = 1; i <= MAX; i++) {
+        for (int i = 1; i <= max; i++) {
             g[i] = g[i - 1] + f[i];
         }
 
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int t = 0; t < T; t++) {
+        for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
             sb.append(g[N]).append("\n");
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
